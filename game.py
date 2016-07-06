c3a119e2d94d55c5b9c5d6fddcb3dd82cbce0d58
Hackbright Student <info@hackbrightacademy.com>
2016-07-06 12:46:43 -0700

    Added loop that compares guess to correct generated random number.

--- game.py
+++ game.py
@@ -1,3 +1,18 @@
 # Put your code here
+
+import random
+
 name = raw_input("Hi! What's your name? ")
 print "Hi, %s" % name
+guess = raw_input("Please guess a number between 1 and 100! ")
+
+correct_guess = random.randint(1, 100)
+
+while correct_guess != int(guess):
+    if int(guess) > correct_guess:
+        guess = raw_input("This is too high. Please guess again. ")
+    else:
+        guess = raw_input("This is too low. Please guess again.")
+
+print "You guessed the correct number!"
+
