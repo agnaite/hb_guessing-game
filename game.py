fe1e074e4c4625c010514452574f82867e122f14
Hackbright Student <info@hackbrightacademy.com>
2016-07-06 12:54:29 -0700

    Added a count for the number of tries the user took to guess the correct number.

--- game.py
+++ game.py
@@ -5,14 +5,17 @@ import random
 name = raw_input("Hi! What's your name? ")
 print "Hi, %s" % name
 guess = raw_input("Please guess a number between 1 and 100! ")
+count = 1
 
 correct_guess = random.randint(1, 100)
 
 while correct_guess != int(guess):
     if int(guess) > correct_guess:
         guess = raw_input("This is too high. Please guess again. ")
+        count += 1
     else:
-        guess = raw_input("This is too low. Please guess again.")
+        guess = raw_input("This is too low. Please guess again. ")
+        count += 1
 
-print "You guessed the correct number!"
+print "You guessed the correct number! It took you %d tries to guess the correct number!" % count
 
